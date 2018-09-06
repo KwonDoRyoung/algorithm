513242d63b16e01451fb25fb2f44e6593ba60f68
ABRDoyoung <entropy0437@gmail.com>
2018-09-06 17:05:28 +0900

    1단계 완성

---
+++ Step01/04_Register.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+import sys
+
+if __name__ == "__main__":
+    sys.stdout.write("{}".format("54\n"))
+    sys.stdout.flush()
+    sys.stdout.write("{}".format("entropy112"))
+    sys.stdout.flush()
---
+++ Step01/05_Dog.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+import sys
+
+if __name__ == "__main__":
+    sys.stdout.write("{}".format('|\_/|\n|q p|   /}\n( 0 )"""\\\n|"^"`    |\n||_/=\\\__|'))
+    sys.stdout.flush()
