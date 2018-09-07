a0eb87fc92cfcfbac84d9770090935b8e7b6dccf
ABRDoyoung <entropy0437@gmail.com>
2018-09-07 17:46:08 +0900

    오류 수정완료

--- Step03/12_Fast_A_add_B.py
+++ Step03/12_Fast_A_add_B.py
@@ -2,6 +2,6 @@ import sys
 
 if __name__ == "__main__":
     T = int(sys.stdin.readline())
-    for _ in range(T):
+    for _ in xrange(T):
         n = sys.stdin.readline().split()
         print(int(n[0]) + int(n[1]))
